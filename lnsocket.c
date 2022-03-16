bb23d0f35f94527e2cb3281dd00dd327d73855d7
William Casarin <jb55@jb55.com>
2022-03-16 09:45:16 -0700

    fix some warnings
    Signed-off-by: William Casarin <jb55@jb55.com>

--- lnsocket.c
+++ lnsocket.c
@@ -104,13 +104,13 @@ static int read_all(int fd, void *data, size_t size)
 
 int EXPORT lnsocket_make_default_initmsg(unsigned char *msgbuf, int buflen)
 {
-	u8 tlvbuf[1024];
-	struct tlv network_tlv;
 	u8 global_features[2] = {0};
 	u8 features[5] = {0};
 	u16 len;
 
 	/*
+	struct tlv network_tlv;
+	u8 tlvbuf[1024];
 	const u8 genesis_block[]  = {
 	  0x6f, 0xe2, 0x8c, 0x0a, 0xb6, 0xf1, 0xb3, 0x72, 0xc1, 0xa6, 0xa2, 0x46,
 	  0xae, 0x63, 0xf7, 0x4f, 0x93, 0x1e, 0x83, 0x65, 0xe1, 0x5a, 0x08, 0x9c,
