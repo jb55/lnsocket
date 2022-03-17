29c8d5e568af782ef011c8625a0b0c4068838693
William Casarin <jb55@jb55.com>
2022-03-16 19:01:41 -0700

    Revert "I prefer this"
    changed my mind again lol
    
    This reverts commit 0bd5a8989512e2c78291f0ea361ab37759dde4b7.

--- examples/node.js
+++ examples/node.js
@@ -1,8 +1,7 @@
 const lnsocket_init = require('lnsocket')
 
 async function go() {
-	const LNSocket = await lnsocket_init()
-	const ln = LNSocket()
+	const ln = await LNSocket()
 	
 	ln.genkey()
 	await ln.connect_and_init("03f3c108ccd536b8526841f0a5c58212bb9e6584a1eb493080e7c1cc34f82dad71", "24.84.152.187")
