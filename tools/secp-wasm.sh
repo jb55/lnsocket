8ea374f8a5a53c4bf5067e512bb2ab8581f05a92
William Casarin <jb55@jb55.com>
2022-07-15 13:24:59 -0700

    update secp and remove old patch
    Signed-off-by: William Casarin <jb55@jb55.com>

--- tools/secp-wasm.sh
+++ tools/secp-wasm.sh
@@ -12,8 +12,6 @@ mkdir -p $PREFIX || exit 1
 
 make distclean > /dev/null
 
-patch -N -p1 < ../../tools/0001-configure-customizable-AR-and-RANLIB.patch
-
 ./configure --disable-shared \
 	    --disable-tests \
 	    --disable-exhaustive-tests \
